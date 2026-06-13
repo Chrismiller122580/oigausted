eee8ae8aa21a40ee6f68d0b26f7fbcef0999fffa
Chrismiller122580 <chrismiller122580@gmail.com>
2026-06-13 19:03:57 +0000

    chore: remove unused categoryEmojis reducer in /gigs (now using icon-registry)

--- src/app/gigs/page.tsx
+++ src/app/gigs/page.tsx
@@ -18,12 +18,6 @@ function GigsClient() {
 
   const { categories: loadedCategories, loading: catLoading } = useGigCategories();
 
-  // Build emoji map from loaded data for dynamic categories
-  const categoryEmojis = loadedCategories.reduce((acc: Record<string, string>, c) => {
-    acc[c.name] = c.icon;
-    return acc;
-  }, {});
-
   const categoryList = loadedCategories.map((c) => c.name);
 
   const [gigs, setGigs] = useState<any[]>([]);
